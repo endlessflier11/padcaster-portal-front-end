01bb6151119289bca8dfa280d86d915390642ece
endlessflier11 <endlessflier@yahoo.com>
2022-02-13 23:35:32 -0800

    feat(P2-526): add search my media

--- src/components/mediabrowser/grid/MediaBrowserGrid.js
+++ src/components/mediabrowser/grid/MediaBrowserGrid.js
@@ -1,16 +1,21 @@
 import MediaTypes from '../../../types/MediaTypes';
-import styles from './MediaBrowserGrid.module.scss'
+import styles from './MediaBrowserGrid.module.scss';
 import MediaBrowserGridItem from './MediaBrowserGridItem';
 
-const MediaBrowserGrid = ({ media }) => {
+const MediaBrowserGrid = ({ media, onGotoSubFolder }) => {
   return (
     <div className={styles.container}>
       {media.map((mediaItem, i) => {
-        return <MediaBrowserGridItem key={i} {...mediaItem} />
+        return (
+          <MediaBrowserGridItem
+            key={i}
+            {...mediaItem}
+            onGotoSubFolder={onGotoSubFolder}
+          />
+        );
       })}
     </div>
   );
-}
+};
 
 export default MediaBrowserGrid;
-

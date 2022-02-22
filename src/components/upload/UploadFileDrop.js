bc287c7f35149a0b0b79cf07883c205c2b8948a5
endlessflier11 <endlessflier@yahoo.com>
2022-02-22 07:20:29 -0800

    feat(p2-501): working

--- src/components/upload/UploadFileDrop.js
+++ src/components/upload/UploadFileDrop.js
@@ -1,5 +1,5 @@
 import { useRef } from 'react';
-import styles from './UploadFileDrop.module.scss'
+import styles from './UploadFileDrop.module.scss';
 import CloudUploadIcon from '../icons/CloudUploadIcon';
 
 const UploadFileDrop = ({ uploads, setUploads }) => {
@@ -7,9 +7,10 @@ const UploadFileDrop = ({ uploads, setUploads }) => {
 
   function handleFiles(files) {
     // TODO: finish implementing when API is more finalized
-  };
+    console.log('files=', files);
+  }
 
-  const defaultDrag = event => {
+  const defaultDrag = (event) => {
     event.stopPropagation();
     event.preventDefault();
   };
@@ -19,7 +20,7 @@ const UploadFileDrop = ({ uploads, setUploads }) => {
       className={styles.container}
       onDragEnter={defaultDrag}
       onDragOver={defaultDrag}
-      onDrop={event => {
+      onDrop={(event) => {
         event.preventDefault();
         event.stopPropagation();
         if (!event?.dataTransfer?.files) return;
@@ -30,23 +31,19 @@ const UploadFileDrop = ({ uploads, setUploads }) => {
       <CloudUploadIcon />
       <div className={styles.text}>
         Drag and drop files here or
-        <label
-          className={styles.label}
-          htmlFor="file-upload"
-          tabIndex="0"
-          onKeyPress={event => {
-            if (event.key === ' ' || event.key === 'Enter' && fileInputRef.current) {
-              fileInputRef.current.click();
-            }
-          }}
-        > browse for files</label>
-        <input type="file" id="file-upload" ref={fileInputRef}
-          onChange={event => handleFiles(event.target.files)}
+        <label className={styles.label} htmlFor='file-upload' tabIndex='0'>
+          {' '}
+          browse for files
+        </label>
+        <input
+          type='file'
+          id='file-upload'
+          ref={fileInputRef}
+          onChange={(event) => handleFiles(event.target.files)}
         />
       </div>
     </div>
   );
-}
+};
 
 export default UploadFileDrop;
-
--- src/components/upload/UploadModal.js
+++ src/components/upload/UploadModal.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import styles from './UploadModal.module.scss'
+import styles from './UploadModal.module.scss';
 import UploadFileDrop from './UploadFileDrop';
 import UploadList from './UploadList';
 import MinimizeWindowIcon from '../icons/MinimizeWindowIcon';
@@ -12,71 +12,82 @@ const UploadModal = ({ closeModal }) => {
     {
       name: 'Chemistry',
       extension: 'mp4',
-      progress: 50
+      progress: 50,
     },
     {
       name: 'BiologyIntro',
       extension: 'mp4',
-      progress: 20
+      progress: 20,
     },
     {
       name: 'Ecosystems',
       extension: 'jpg',
-      progress: 89
+      progress: 89,
     },
     {
       name: 'Ecosystems',
       extension: 'jpg',
-      progress: 89
+      progress: 89,
     },
     {
       name: 'Ecosystems',
       extension: 'jpg',
-      progress: 89
+      progress: 89,
     },
     {
       name: 'Ecosystems',
       extension: 'jpg',
-      progress: 89
+      progress: 89,
     },
     {
       name: 'Ecosystems',
       extension: 'jpg',
-      progress: 89
+      progress: 89,
     },
     {
       name: 'Ecosystems',
       extension: 'jpg',
-      progress: 89
+      progress: 89,
     },
   ];
 
   const [uploads, setUploads] = useState(defaultUploads);
   const [modalViewType, setModalViewType] = useState(UploadModalTypes.DEFAULT);
 
-  const cancelDownload = index => () => {
+  const cancelDownload = (index) => () => {
     const newUploads = uploads.filter((upload, i) => i !== index);
     setUploads(newUploads);
-  }
+  };
 
   return (
-    <div className={modalViewType === UploadModalTypes.DEFAULT ? styles.containerDefault : styles.containerMinimize}>
+    <div
+      className={
+        modalViewType === UploadModalTypes.DEFAULT
+          ? styles.containerDefault
+          : styles.containerMinimize
+      }
+    >
       <div className={styles.header}>
         <h2 className={styles.title}>Upload Media</h2>
         <div className={styles.icons}>
-          <button
-            className={styles.minimize}
-            onClick={() => setModalViewType(UploadModalTypes.MINIMIZE)}
-          >
-            <MinimizeWindowIcon />
+          {modalViewType === UploadModalTypes.DEFAULT ? (
+            <button
+              className={styles.minimize}
+              onClick={() => setModalViewType(UploadModalTypes.MINIMIZE)}
+            >
+              <MinimizeWindowIcon />
+            </button>
+          ) : (
+            <button
+              className={styles.default}
+              onClick={() => setModalViewType(UploadModalTypes.DEFAULT)}
+            >
+              <MaximizeWindowIcon />
+            </button>
+          )}
+          <button className={styles.close} onClick={closeModal}>
+            <CloseIcon />
           </button>
-          <button
-            className={styles.default}
-            onClick={() => setModalViewType(UploadModalTypes.DEFAULT)}
-          >
-            <MaximizeWindowIcon />
-          </button>
-          <button className={styles.close} onClick={closeModal}><CloseIcon /></button>
         </div>
       </div>
       {modalViewType !== UploadModalTypes.MINIMIZE && (
@@ -87,7 +98,6 @@ const UploadModal = ({ closeModal }) => {
       )}
     </div>
   );
-}
+};
 
 export default UploadModal;
-
